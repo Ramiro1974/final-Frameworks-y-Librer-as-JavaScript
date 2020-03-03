0f3e30a89e68a2488068b3f93aa1da458a26ee91
Ramiro-desarrollo Cr_Consultores <39528343+Ramiro1974@users.noreply.github.com>
2020-03-03 14:19:10 -0500

    cambio de color

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    //window.alert('estoy funcionando');
+    $('.main-titulo').animate({
+        color: "green",
+    },1000,function(){
+        $('.main-titulo').animate({
+            color: "blue",
+        },1000,function(){
+            $('.main-titulo').animate({
+                color: "pink",
+            },1000,function(){
+                $('.main-titulo').animate({
+                    color: "yellow"
+                },1000)
+            })
+        })
+    }
+    )
+});
