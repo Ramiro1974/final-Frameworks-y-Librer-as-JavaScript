b6d3eb77359336ae188a2c3c161912a21fb7caf7
Ramiro-desarrollo Cr_Consultores <39528343+Ramiro1974@users.noreply.github.com>
2021-02-01 15:42:09 -0500

    problema_arreglo_objeto

--- js/app2.js
+++ js/app2.js
@@ -1,16 +1,43 @@
+/** 
+$(document).ready(function(){
+    var cli='';
+    $("img").click(function(){
+        cli=$("img").html();
+        console.log("el dulce es: "+cli);
+     });
+     console.log("el tiempo es: "+$("data-info").html());
+});
+*/
+var igual=[];
 $(document).ready(function(){
     var consulta;
-    $(".dulces").click(function(){
-        consulta= $("img").attr("src");
-        console.log("si esta funcionando");
+    $("img").click(function(){
+        consulta= $(this)
+        eventoDulce(consulta);
         
     });
 });
-function eventoDulce(){
-    console.log(consulta);
+function eventoDulce(cosa){
+    var temp=cosa; 
+    const temp={
+
+    }
+    igual.push(temp);
+    var contadosCondicion=igual.length;
+    if(contadosCondicion != 0 && contadosCondicion >= 2){
+        if(igual[0]===igual[1]){
+            alert("igual");
+        }else{
+            alert("diferente");
+        }
+    }else{
+        alert("se encuentra null");        
+    } 
+    for(var i=0 ; i< igual.length ; i++){
+        console.log("el elemento es: "+igual[i])+"\n";
+    }
+    
 }
-
-
 
 /**
 $(function(){
@@ -25,3 +52,4 @@ $(function(){
     });
  
 });*/
+
