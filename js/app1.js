cafac7b79f11a971c52e71777f8e939d8e8c9031
Ramiro-desarrollo Cr_Consultores <39528343+Ramiro1974@users.noreply.github.com>
2021-02-21 20:20:56 -0500

    llanado sin columnas

--- js/app1.js
+++ js/app1.js
@@ -5,17 +5,18 @@ const dulces=['image/1.png','image/2.png','image/3.png','image/4.png'];
 const coma=" ' ";
 const cssDulce='style="margin-bottom: 15px;"';
 $(function(){
-    var id=1;
-    //$(".panel-tablero").attr("id","dulce");
-    for( var h=1;h<8;h++){         
-        for(var i=1;i<6;i++){
-            colum=".col-"+h;           
-            dulce= Math.floor(Math.random() * (4 - 0) ) + 0;            
+    for(var n=1;n<8;n++){
+        colum=".col-"+n;
+        $(colum).remove();
+    }
+    $(".panel-tablero").html(function(){
+        var id=1;
+        for(var h=1;h<25;h++){
+            dulce= Math.floor(Math.random() * (4 - 0) ) + 0;
             z='<img id="'+id+'" '+cssDulce+'src="'+dulces[dulce]+'" class="dulces" onclick="eventoDulce('+coma+dulce+coma+','+coma+colum+coma+')">';
             dulceslleno.push(z);
-            $(colum).append(dulceslleno[id-1]);
-            $(colum).attr("id","dulcess");
-            id=id+1;            
-        }  
-    }
+            $(this).append(dulceslleno[id-1]);
+            id=id+1;
+        }
+    });
 });
