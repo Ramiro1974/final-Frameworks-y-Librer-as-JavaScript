eebde66fe43bb1fa20d495352d4164c10ed595d8
Ramiro-desarrollo Cr_Consultores <39528343+Ramiro1974@users.noreply.github.com>
2021-02-24 22:01:06 -0500

    reordenamiento codigo

--- js/app1.js
+++ js/app1.js
@@ -1,22 +1,20 @@
-var z='';
-var dulceslleno=[];
-var colum=0;
-const dulces=['image/1.png','image/2.png','image/3.png','image/4.png'];
-const coma=" ' ";
-const cssDulce='style="margin-bottom: 15px;"';
-$(function(){
-    for(var n=1;n<8;n++){
-        colum=".col-"+n;
-        $(colum).remove();
-    }
-    $(".panel-tablero").html(function(){
-        var id=1;
-        for(var h=1;h<25;h++){
-            dulce= Math.floor(Math.random() * (4 - 0) ) + 0;
-            z='<img id="'+id+'" '+cssDulce+'src="'+dulces[dulce]+'" class="dulces" onclick="eventoDulce('+coma+dulce+coma+','+coma+colum+coma+')">';
+var z = '';
+var dulceslleno = [];
+var colum = 0;
+const dulces = ['image/1.png', 'image/2.png', 'image/3.png', 'image/4.png'];
+const coma = " ' ";
+const cssDulce = 'style="margin-bottom: 15px;"';
+$(function() {
+    var id = 1;
+    for (var n = 1; n < 8; n++) {
+        colum = ".col-" + n;
+        for (var h = 1; h < 6; h++) {
+            dulce = Math.floor(Math.random() * (4 - 0)) + 0;
+            z = '<img id="' + id + '" ' + cssDulce + 'src="' + dulces[dulce] + '" class="dulces" onclick="eventoDulce(' + coma + dulce + coma + ',' + coma + colum + coma + ')">';
             dulceslleno.push(z);
-            $(this).append(dulceslleno[id-1]);
-            id=id+1;
+            $(colum).append(dulceslleno[id - 1]);
+            id = id + 1;
         }
-    });
+
+    }
 });
--- js/app2.js
+++ js/app2.js
@@ -1,35 +1,6 @@
-
-var col;
-var igual=[];
-
-$("#dulcess").sortable();
-
-$(document).ready(function(){
-    var consulta;
-    $("img").click(function(){
-    consulta= $(this);
-    });
-    console.log(consulta);
-});
-
-function eventoDulce(cosa, col){
-    
-    var temp=cosa;
-    igual.push(temp);
-    var contadosCondicion=igual.length;
-    if(contadosCondicion != 0 && contadosCondicion < 4){
-        if(igual[0]===igual[1]===igual[3]){
-
-        }else{
-            console.log("diferente");
-        }
-    }else{
-        igual.splice(0,3);
-        igual.pop();
-        //alert("*** condicion no cumple ****");        
-    } 
-    igual.forEach(function(contenido){
-        console.log(contenido);
-    });
-    console.log(igual.length);
-}
+$(
+    function() {
+        $("img").draggable();
+        $("img").draggable();
+    }
+);
